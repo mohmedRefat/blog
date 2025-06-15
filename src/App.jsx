6050e3114ee07239f9b2281c8d5c55418adb4310
Mohmed <mezoxmoonx@gmail.com>
2025-06-15 17:56:33 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,8 @@ import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
-    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64"> 
-    {/* Navbar */}
-    <Navbar/>
-    {/* bread crumb */}
-    {/* intro  */}
-    {/* features posts  */}
-    {/* posts list  */}
-    
+    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+      <Navbar />
     </div>
   );
 }
